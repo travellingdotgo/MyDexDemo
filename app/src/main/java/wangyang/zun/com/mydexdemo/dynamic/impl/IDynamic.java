982ce489cf069ea6090c13eac0641942d9e65f6d
c <c@g.hk>
2018-06-21 16:09:35 +0800

    1. code cleanup; 2. add implementation

---
+++ app/src/main/java/wangyang/zun/com/mydexdemo/dynamic/impl/IDynamic.java
@@ -0,0 +1,14 @@
+package wangyang.zun.com.mydexdemo.dynamic.impl;
+
+import wangyang.zun.com.mydexdemo.dynamic.Dynamic;
+
+/**
+ * Created by user on 6/21/18.
+ */
+
+public class IDynamic implements Dynamic{
+
+    public String sayHelloy(){
+        return "Woshi heihei haha kjlkjklj";
+    }
+}
